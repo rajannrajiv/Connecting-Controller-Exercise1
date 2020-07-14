d382119c8b25d5f536ebb5719e4de7b90e903d5e
rajiv <rs481441@gmail.com>
2020-07-14 23:45:19 +0530

    Initial commit

---
+++ src/main/java/com/udacity/Connecting/Controller/Exercise/ConnectingControllerExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.Connecting.Controller.Exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectingControllerExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectingControllerExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udacity/Connecting/Controller/Exercise/ConnectingControllerExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.Connecting.Controller.Exercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectingControllerExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
